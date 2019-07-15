97f3b073b200ea4500f2b70e1180c6e801c65b9f
Deepesh Krishnan <deepesh.krishnan@gmail.com>
2019-07-15 13:38:34 -0300

    completed app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
-import {SIGN_IN, SIGN_OUT} from './type'
+import streams from '../apis/streams';
+import createBrowserHistory from '../history';
+
+import {SIGN_IN, SIGN_OUT, 
+  CREATE_STREAM, 
+  FETCH_STREAMS,
+  FETCH_STREAM, 
+  EDIT_STREAM, 
+  DELETE_STREAM } from './type'
 
 export const signIn = (userId) => {
   return {
@@ -12,3 +20,37 @@ export const signOut = () => {
     type: SIGN_OUT
   }
 }
+
+// post new
+export const createStream = formValues => async (dispatch, getState) => {
+ const {userId} = getState().auth;
+ const response = await streams.post('/streams', {...formValues, userId})
+ dispatch({ type: CREATE_STREAM, payload: response.data})
+ // if success then navigate out
+ createBrowserHistory.push('/');
+};
+
+// get all
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams')
+  // console.log(`List of streams in actions: ${JSON.stringify(response.data)}`)
+  dispatch({ type: FETCH_STREAMS, payload: response.data})
+ };
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`)
+  dispatch({type: FETCH_STREAM, payload: response.data})
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.patch(`/streams/${id}`, formValues)
+  dispatch({type: EDIT_STREAM, payload: response.data})
+  createBrowserHistory.push('/');
+}
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`)
+  dispatch({type: DELETE_STREAM, payload: id})
+  createBrowserHistory.push('/');
+}
+
---
+++ src/history.js
@@ -0,0 +1,4 @@
+import { createBrowserHistory } from 'history'; 
+// auto installed by react router dom
+
+export default createBrowserHistory();
